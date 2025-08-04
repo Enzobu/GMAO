044d1d4411d101750d8774adc5a213fc0ad809ef
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-08-04 10:38:49 +0200

    Add navBar

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(
+        #[CurrentUser] User $currentUser,
+    ): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'currentUser' => $currentUser,
+        ]);
+    }
+}
