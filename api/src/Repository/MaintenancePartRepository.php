5a1e1e54198a802da619f37a2b18dbae5b256d45
Enzo Palermo <palermo.enzo.ep@gmail.com>
2026-05-29 21:22:42 +0200

    refactor: remove unless line in repositories

--- api/src/Repository/MaintenancePartRepository.php
+++ api/src/Repository/MaintenancePartRepository.php
@@ -15,29 +15,4 @@ class MaintenancePartRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, MaintenancePart::class);
     }
-
-    //    /**
-    //     * @return MaintenancePart[] Returns an array of MaintenancePart objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?MaintenancePart
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
--- api/src/Repository/PartTypeRepository.php
+++ api/src/Repository/PartTypeRepository.php
@@ -15,29 +15,4 @@ class PartTypeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, PartType::class);
     }
-
-    //    /**
-    //     * @return PartType[] Returns an array of PartType objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?PartType
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
