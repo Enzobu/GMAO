51fd5a8ac26bea32f2d0a60ac3dc6b4ab8b3cc27
Enzo Palermo <palermo.enzo.ep@gmail.com>
2026-05-29 21:45:24 +0200

    refactor: remove unless import

--- api/src/Service/MailManager.php
+++ api/src/Service/MailManager.php
@@ -7,12 +7,8 @@ use Symfony\Component\Mailer\MailerInterface;
 
 final readonly class MailManager
 {
-    private String $mailerSender;
 
     public function __construct(
-        private MailerInterface $mailerInterface,
         private ContainerBagInterface $params,
-    ) {
-        $this->mailerSender = $this->params->get('mailer_sender');
-    }
+    ) {}
 }
